04475397c9d988f591ce807cf330c18be85245f7
Seok_Soo <whdtn7855@naver.com>
2025-06-13 18:07:10 +0900

    Initial Commit

---
+++ MyStudyPlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStudyPlan
+//
+//  Created by 석종수 on 6/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
