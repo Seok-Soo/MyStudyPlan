c4f0883ee2f59a51b04eea66240ed91089359b3f
Seok_Soo <whdtn7855@naver.com>
2025-06-13 20:42:27 +0900

    탭바 구성 및 완성

---
+++ MyStudyPlan/TodoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodoViewController.swift
+//  MyStudyPlan
+//
+//  Created by 석종수 on 6/13/25.
+//
+
+import UIKit
+
+class TodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
