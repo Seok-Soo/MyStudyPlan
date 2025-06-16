b9c727d88540d01be42ff3bf2cc0ba19e22612e9
Seok_Soo <whdtn7855@naver.com>
2025-06-16 21:06:13 +0900

    일정 추가 화면 수정

--- MyStudyPlan/TodoAddViewController.swift
+++ MyStudyPlan/TodoAddViewController.swift
@@ -1,53 +1,74 @@
-//
-//  TodoAddViewController.swift
-//  MyStudyPlan
-//
-//  Created by 석종수 on 6/16/25.
-//
-
 import UIKit
 
-class TodoAddViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    
+class TodoAddViewController: UIViewController {
+
     // MARK: - IBOutlet 연결
     @IBOutlet weak var titleField: UITextField!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var statusPicker: UIPickerView!
-    
-    // 상태 목록
-    let statuses = ["시작전", "진행중", "완료"]
-    
-    // 부모로 전달할 콜백
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var statusSegment: UISegmentedControl!
+
+    // 내부 datePicker
+    let datePicker = UIDatePicker()
+
+    // 콜백
     var onAddTodo: ((TodoItem) -> Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // PickerView 연결
-        statusPicker.dataSource = self
-        statusPicker.delegate = self
-        
-        // 날짜 설정
-        datePicker.datePickerMode = .date
-        datePicker.preferredDatePickerStyle = .wheels
+        setupDatePicker()
+        setupSegmentStyle()
     }
 
-    // MARK: - 저장 액션
-    @IBAction func didTapSave(_ sender: UIButton) {
-        guard let title = titleField.text, !title.isEmpty else { return }
+    // MARK: - 날짜 Picker 설정
+    func setupDatePicker() {
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.locale = Locale(identifier: "ko_KR")
+        dateTextField.inputView = datePicker
 
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        toolbar.items = [
+            UIBarButtonItem(title: "완료", style: .plain, target: self, action: #selector(doneDatePicker))
+        ]
+        dateTextField.inputAccessoryView = toolbar
+    }
+
+    @objc func doneDatePicker() {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
-        let dateString = formatter.string(from: datePicker.date)
+        dateTextField.text = formatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
 
-        let selectedRow = statusPicker.selectedRow(inComponent: 0)
-        let selectedStatus = statuses[selectedRow]
+    // MARK: - 세그먼트 스타일 설정
+    func setupSegmentStyle() {
+        statusSegment.removeAllSegments()
+        let statuses = ["시작전", "진행중", "완료"]
+        for (index, title) in statuses.enumerated() {
+            statusSegment.insertSegment(withTitle: title, at: index, animated: false)
+        }
+        statusSegment.selectedSegmentIndex = 0
+
+        // 선택된 항목 스타일
+        statusSegment.selectedSegmentTintColor = .systemBlue
+        statusSegment.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+        statusSegment.setTitleTextAttributes([.foregroundColor: UIColor.gray], for: .normal)
+    }
+
+    // MARK: - 저장
+    @IBAction func didTapSave(_ sender: UIButton) {
+        guard let title = titleField.text,
+              let date = dateTextField.text,
+              !title.isEmpty, !date.isEmpty else { return }
+
+        let selectedStatus = statusSegment.titleForSegment(at: statusSegment.selectedSegmentIndex) ?? "시작전"
 
         let newTodo = TodoItem(
             id: UUID().uuidString,
             title: title,
             status: selectedStatus,
-            date: dateString,
+            date: date,
             duration: 3600 // 기본값: 1시간
         )
 
@@ -58,17 +79,5 @@ class TodoAddViewController: UIViewController, UIPickerViewDataSource, UIPickerV
     @IBAction func didTapCancel(_ sender: UIButton) {
         dismiss(animated: true)
     }
-
-    // MARK: - UIPickerView DataSource & Delegate
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return statuses.count
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return statuses[row]
-    }
 }
+	
